564aec3df45dd1d57f62028624ae1a6e7015a916
Ramoxxx <leogrdss@icloud.com>
2020-07-01 23:14:03 -0300

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom'
 
-import Header  from "./components/Header/Header.jsx"
+import Header  from "./components/header/Header"
 
 import Series from "./pages/series/Series.jsx"
 import About from "./pages/about/About.jsx"
