7333f37fa1e55a67d7732e5100dd8b4403db7ce3
Ramoxxx <leogrdss@icloud.com>
2020-07-02 21:24:05 -0300

    fixing cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,13 +7,13 @@ export default function CardSerie({ serie }) {
     const imgUrl = "https://image.tmdb.org/t/p/w200/"
     const data = new Date(serie.first_air_date)
     const data_lancamento = data.toLocaleDateString()
+
   return (
     <div className="card">
       <span className="material-icons fav-btn">add_circle_outline</span>
       <div
        className="poster-image"
-       style={{ backgroundImage: `url(${imgUrl}${serie.poster_path})`
-       } }
+       style={{ backgroundImage: `url(${imgUrl}${serie.poster_path})`} }
        >
       </div>
       <div className="card-detail">
@@ -23,12 +23,8 @@ export default function CardSerie({ serie }) {
   <h1 className="title">{serie.name}</h1>
   <h2>{data_lancamento}</h2>
       </div>
-      <div className="button">
       <a href="#" className="btn-detail">
-        <span className="material-icons">play_circle_filled</span>
-        Details
-      </a>
+        <span className="material-icons">play_circle_filled</span>Details</a>
       </div>
-    </div>
   );
 }
